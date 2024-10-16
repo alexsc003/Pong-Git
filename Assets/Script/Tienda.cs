f7256f838aec0b9804dea8d84fdcabd39bc95a9e
alexsc003 <alexnebra03@gmail.com>
2024-10-16 23:54:36 +0200

    ds

--- Assets/Script/Tienda.cs
+++ Assets/Script/Tienda.cs
@@ -7,15 +7,23 @@ public class Tienda : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI dinero;
+    
     int dineroCantidad;
+    
     [SerializeField]
     TextMeshProUGUI precio;
+    [SerializeField]
+    TextMeshProUGUI precio2;
     int precioCantidad;
+    int precio2Cantidad;
+
     void Start()
     {
         dineroCantidad = Random.Range(0,100);
         dinero.text = dineroCantidad.ToString();
-        dineroCantidad = Random.Range(0, 50);
+        precioCantidad = Random.Range(0,50);
         precio.text = precioCantidad.ToString();
+        precio2Cantidad = Random.Range(0, 50);
+        precio2.text = precioCantidad.ToString();
     }
 }
